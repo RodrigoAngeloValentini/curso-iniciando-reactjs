c4eaa74ea1618ecd23bf20caa088aa26d46c96ad
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2017-08-13 20:03:15 -0300

    ajustes

---
+++ iniciando-react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(
+    <h1>Hello World from Component React.js by School of net' </h1>,
+    document.getElementById("root")
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
